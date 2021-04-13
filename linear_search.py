1e00dc4054d223612d576588f566a710f54de95a
anwu1219 <haozewu@stanford.edu>
2021-04-12 17:06:44 -0700

    clean up

---
+++ linear_search.py
@@ -0,0 +1,26 @@
+import sys
+import subprocess
+import os
+
+runScript = sys.argv[1]
+cw_summary_file = sys.argv[2]
+summary_folder = sys.argv[3]
+args = sys.argv[4:]
+
+upperEps = float(open(cw_summary_file).read().split()[1])
+
+eps_high = upperEps
+
+while eps_high > 0:
+    eps_high -= 0.02 * upperEps
+    currentEps = eps_high
+    summary_file = summary_folder + "/{}.summary".format(currentEps)
+    command = "{} {} --epsilon {} --summary-file {}".format(runScript, " ".join(args), currentEps, summary_file).split()
+    subprocess.run(command)
+    assert(os.path.isfile(summary_file))
+    result = open(summary_file, 'r').readlines()[0].split()[0]
+    if (result == 'sat'):
+        continue
+    else:
+        assert(result == "unsat")
+        exit(0)
